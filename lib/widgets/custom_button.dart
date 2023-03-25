fde86a05d88285a10dd658014c56c24eafa8e81b
hillaryomondi <hillary.okoth@strathmore.edu>
2023-03-25 22:57:18 +0300

    Added login screen design

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meeting_app/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
@@ -9,12 +10,26 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 17,
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 17,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor),
+          ),
         ),
       ),
     );
